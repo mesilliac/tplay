7bbc04cafcb4b452ff5fc786beb6161fe093d484
Tommy <mesilliac@gmail.com>
2012-06-23 21:59:54 +1200

    add module for custom python instruments.
    * including basic string synth.

---
+++ instruments.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from __future__ import division
+import Nsound
+from random import random
+import collections
+
+class Koto():
+    "Nsound-compatible plucked-string instrument based on Karplus-Strong"
+    def __init__(self,samplerate):
+        self.samplerate = samplerate
+    def play(self,duration,frequency):
+        samplerate = self.samplerate
+        out = Nsound.AudioStream(samplerate,1) # mono
+        cycles = int(samplerate/frequency)
+        ring = collections.deque(Nsound.Buffer.rand(cycles).toList())
+        for i in xrange(int(samplerate*duration)):
+            out << ring[0]
+            avg = 0.996*0.5*(ring[0] + ring[1])
+            ring.append(avg)
+            ring.popleft()
+        return out
+
